e0160fcc6192eba372843166a550532ca7b56a8e
Arhum Savera <arhumsavera@1x-nat-vl931-172-30-183-156.wireless.umass.edu>
2018-05-16 01:51:16 -0400

    updated site with static files

--- blog/models.py
+++ blog/models.py
@@ -9,6 +9,10 @@ class Blog(models.Model):
 	body=models.TextField()
 	image=models.ImageField(upload_to='images/')
 
+	def summary(self):
+		return self.body[:30]+'...'
 
+	def pub_date_pretty(self):
+		return self.pubdate.strftime('%b %e %Y')
 	def __str__(self):
 		return self.title
