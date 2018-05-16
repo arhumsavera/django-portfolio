d8e4b6da625831b8b577e35c52c7ce41e0ba2c88
Arhum Savera <arhumsavera@1x-nat-vl931-172-30-183-156.wireless.umass.edu>
2018-05-16 02:02:25 -0400

    final touches

--- blog/views.py
+++ blog/views.py
@@ -8,5 +8,6 @@ def allblogs(request):
 	return render (request, 'blog/blogs.html', {'blogs': blogs})
 
 def detail(request, blog_id):
+	print('got here!')
 	blog=get_object_or_404(Blog, pk=blog_id)
 	return render (request, 'blog/detail.html', {'blog': blog})
