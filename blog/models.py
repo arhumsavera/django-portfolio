4a8d275895fd6763fc5799cc81a049f2d7ceb662
Arhum Savera <arhumsavera@1x-nat-vl931-172-30-183-156.wireless.umass.edu>
2018-05-16 00:48:11 -0400

    set up portfolio site

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
+
+class Blog(models.Model):
+	title=models.CharField(max_length=200)
+	pubdate=models.DateTimeField(default=datetime.now, blank=True)
+	body=models.TextField()
+	image=models.ImageField(upload_to='images/')
+
+
+	def __str__(self):
+		return self.title
--- jobs/models.py
+++ jobs/models.py
@@ -1,8 +1,14 @@
 from django.db import models
-
+from PIL import Image
+from io import BytesIO
+from django.core.files.uploadedfile import InMemoryUploadedFile
+import sys
 # Create your models here.
 
 class Job(models.Model):
 	image=models.ImageField(upload_to='images/')
 	summary=models.CharField(max_length=250)
 
+
+	def __str__(self):
+		return self.summary
